ec3dbd65677b937392a499ec094e455b3fcd9fe4
unknown <adewaleadeboye10@gmail.com>
2023-09-13 16:13:35 +0100

    updated

---
+++ client/src/components/section/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="myname uppercase">
+      All rights reserved. Adewale Victor Adeboye{" "}
+      <span className="text-[#469DF0]">regal</span>
+    </div>
+  );
+};
+
+export default Footer;
